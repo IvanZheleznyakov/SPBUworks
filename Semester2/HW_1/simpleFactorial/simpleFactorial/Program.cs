9c9b98f0126667815c0e861f51d39657c54a17ab
IvanZheleznyakov <iz14@yandex.ru>
2015-03-23 22:36:44 +0400

    start of my way

---
+++ Semester2/HW_1/simpleFactorial/simpleFactorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleFactorial
+{
+    class Program
+    {
+        static int Factorial(int number)
+        {
+            int result = 1;
+            for (int i = 1; i != number + 1; ++i)
+            {
+                result *= i;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter natural number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Factorial is " + Factorial(number));
+        }
+    }
+}
