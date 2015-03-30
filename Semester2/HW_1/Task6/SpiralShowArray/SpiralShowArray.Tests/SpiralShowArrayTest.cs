ad8ec01d4311e2efff9e6a04067d84767dfc2db1
IvanZheleznyakov <iz14@yandex.ru>
2015-03-30 15:43:02 +0400

    Added unit test

--- Semester2/HW_1/Task6/SpiralShowArray/SpiralShowArray.Tests/SpiralShowArrayTest.cs
+++ Semester2/HW_1/Task6/SpiralShowArray/SpiralShowArray.Tests/SpiralShowArrayTest.cs
@@ -9,7 +9,13 @@ namespace SpiralShowArray.Tests
         [TestMethod]
         public void SpiralShowMatrixTest()
         {
-
+            const int sizeOfMatrix = 3;
+            int[][] matrix = new int[sizeOfMatrix][];
+            matrix[0] = new int[] { 0, 1, 2 };
+            matrix[1] = new int[] { 1, 2, 3 };
+            matrix[2] = new int[] { 2, 3, 4 };
+            string testString = "2 3 4 3 2 1 0 1 2 ";
+            Assert.AreEqual(testString, SpiralShowArray.Program.SpiralShowMatrix(matrix));
         }
     }
 }
--- Semester2/HW_1/Task6/SpiralShowArray/SpiralShowArray/Program.cs
+++ Semester2/HW_1/Task6/SpiralShowArray/SpiralShowArray/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SpiralShowArray
 {
-    class Program
+    public class Program
     {
         /// <summary>
         /// Function show the matrix spirally.
