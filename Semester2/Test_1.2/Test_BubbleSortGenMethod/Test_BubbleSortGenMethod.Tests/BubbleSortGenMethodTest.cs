4754c297cb3dae340c5fde4e62f71003a0e68718
IvanZheleznyakov <iz14@yandex.ru>
2015-04-24 17:08:07 +0400

    Add test

---
+++ Semester2/Test_1.2/Test_BubbleSortGenMethod/Test_BubbleSortGenMethod.Tests/BubbleSortGenMethodTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_BubbleSortGenMethod.Tests
+{
+    [TestClass]
+    public class BubbleSortGenMethodTest
+    {
+        [TestMethod]
+        public void BubbleSortIntTest()
+        {
+            const int sizeOfList = 5;
+            List<int> list = new List<int>(sizeOfList);
+            list.Add(0);
+            list.Add(-2);
+            list.Add(-5);
+            list.Add(7);
+            list.Add(6);
+            List<int> needList = new List<int>(list);
+            needList.Sort();
+            Comparer<int> comparer = Comparer<int>.Default;
+            Test_BubbleSortGenMethod.Program.BubbleSort<int>(list, comparer);
+            for (int i = 0; i != sizeOfList; ++i)
+            {
+                Assert.AreEqual(needList[i], list[i]);
+            }
+        }
+
+        [TestMethod]
+        public void BubbleSortStringTest()
+        {
+            const int sizeOfList = 7;
+            List<string> list = new List<string>(sizeOfList);
+            list.Add("cbba");
+            list.Add("aaaa");
+            list.Add("aaba");
+            list.Add("cabb");
+            list.Add("aaaaa");
+            list.Add("babc");
+            list.Add("cabb");
+            List<string> needList = new List<string>(list);
+            needList.Sort();
+            Comparer<string> comparer = Comparer<string>.Default;
+            Test_BubbleSortGenMethod.Program.BubbleSort<string>(list, comparer);
+            for (int i = 0; i != sizeOfList; ++i)
+            {
+                Assert.AreEqual(needList[i], list[i]);
+            }
+        }
+    }
+}
---
+++ Semester2/Test_1.2/Test_BubbleSortGenMethod/Test_BubbleSortGenMethod/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_BubbleSortGenMethod
+{
+    public class Program
+    {
+        /// <summary>
+        /// Swap two elements in list.
+        /// </summary>
+        public static List<T> SwapInList<T>(List<T> list, int firstIndex, int secondIndex)
+        {
+            T temp = list[firstIndex];
+            list[firstIndex] = list[secondIndex];
+            list[secondIndex] = temp;
+            return list;
+        }
+
+        /// <summary>
+        /// Sort elements in list with bubble sort.
+        /// </summary>
+        public static List<T> BubbleSort<T>(List<T> list, Comparer<T> comparer)
+        {
+            int sizeOfList = list.Count<T>();
+            for (int i = 0; i != sizeOfList - 1; ++i)
+            {
+                for (int j = 0; j != sizeOfList - i - 1; ++j)
+                {
+                    if (comparer.Compare(list[j], list[j + 1]) > 0)
+                    {
+                        list = SwapInList<T>(list, j, j + 1);
+                    }
+                }
+            }
+                return list;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
