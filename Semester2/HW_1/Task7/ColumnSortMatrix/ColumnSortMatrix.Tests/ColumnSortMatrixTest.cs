73af29ab9766c9fb95c1e68dac7c19cd6f95b243
IvanZheleznyakov <iz14@yandex.ru>
2015-05-06 22:18:51 +0400

    fixed unit test

--- Semester2/HW_1/Task7/ColumnSortMatrix/ColumnSortMatrix.Tests/ColumnSortMatrixTest.cs
+++ Semester2/HW_1/Task7/ColumnSortMatrix/ColumnSortMatrix.Tests/ColumnSortMatrixTest.cs
@@ -17,12 +17,18 @@ namespace ColumnSortMatrix.Tests
             };
             int[][] rightArray =
             {
-                new int[] { 3, 5, 9, 8 },
+                new int[] { 3, 5, 8, 9 },
                 new int[] { 6, 7, 8, 2 },
                 new int[] { 1, 1, 1, 1 }
             };
             ColumnSortMatrix.Program.ColumnSelectionSort(array);
-            Assert.AreEqual(rightArray, array);
+            for (int i = 0; i != 3; ++i)
+            {
+                for (int j = 0; j != 4; ++j)
+                {
+                    Assert.AreEqual(rightArray[i][j], array[i][j]);
+                }
+            }
         }
     }
 }
