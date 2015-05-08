c94e59398399d2da49e1f0531e38ecf35d9b0bfd
IvanZheleznyakov <iz14@yandex.ru>
2015-05-09 01:26:34 +0400

    stack without unit-tests

---
+++ Semester2/HW_2/Task1/StackClass/StackClass/StackClass/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackClass
+{
+    public class Stack
+    {
+        private class StackElement
+        {
+            private int _value;
+            public int Value
+            {
+                get
+                {
+                    return _value;
+                }
+                set
+                {
+                    _value = value;
+                }
+            }
+            public StackElement Next { get; set; }
+        }
+
+        public void Push(int value)
+        {
+            var newElement = new StackElement()
+            {
+                Next = head,
+                Value = value
+            };
+            head = newElement;
+        }
+
+        public int Pop()
+        {
+            var temp = head.Value;
+            head = head.Next;
+            return temp;
+        }
+
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+
+        private StackElement head = null;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
