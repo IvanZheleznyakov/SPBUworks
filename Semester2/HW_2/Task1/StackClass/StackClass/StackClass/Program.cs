c9aa0ec0c94269e05a79b4720446d78c4d512f5c
IvanZheleznyakov <iz14@yandex.ru>
2015-05-10 18:18:13 +0400

    Added comments to methods

--- Semester2/HW_2/Task1/StackClass/StackClass/StackClass/Program.cs
+++ Semester2/HW_2/Task1/StackClass/StackClass/StackClass/Program.cs
@@ -25,6 +25,10 @@ namespace StackClass
             public StackElement Next { get; set; }
         }
 
+        /// <summary>
+        /// Add element to stack.
+        /// </summary>
+        /// <param name="value"></param>
         public void Push(int value)
         {
             var newElement = new StackElement()
@@ -35,6 +39,10 @@ namespace StackClass
             head = newElement;
         }
 
+        /// <summary>
+        /// Remove element from stack, if it possible.
+        /// </summary>
+        /// <returns></returns>
         public int Pop()
         {
             if (head == null)
@@ -46,6 +54,10 @@ namespace StackClass
             return temp;
         }
 
+        /// <summary>
+        /// Check if stack is empty.
+        /// </summary>
+        /// <returns></returns>
         public bool IsEmpty()
         {
             return head == null;
