ee9698d97f98b48ee13f9a9f2049fc7cc686bed4
IvanZheleznyakov <iz14@yandex.ru>
2015-05-11 18:26:29 +0400

    Made hashtable with using linked list, without tests

---
+++ Semester2/HW_2/Task3/HashTableClass/HashTableClass/HashTableClass/HashTable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableClass
+{
+    class HashTable
+    {
+        HashTable(int capacity = 50)
+        {
+            Capacity = capacity;
+        }
+
+        /// <summary>
+        /// Value of hash function for specified string.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public int HashFunction(string value)
+        {
+            int result = 0;
+            for (int i = 0; i != value.Length; ++i)
+            {
+                result += (int)value[i];
+            }
+            return result % Capacity;
+        }
+
+        /// <summary>
+        /// Add element to hashtable.
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddHashElement(string value)
+        {
+            int index = HashFunction(value);
+            HashTable[index].AddUniqueElement(value);
+        }
+
+        /// <summary>
+        /// Check if element with specified value in hashtable.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool IsElementInHashTable(string value)
+        {
+            int index = HashFunction(value);
+            return HashTable[index].IsElementInList(value);
+        }
+        public static int Capacity { get; private set; }
+        public LinkedList<string>[] HashTable = new LinkedList<string>[Capacity];
+    }
+}
---
+++ Semester2/HW_2/Task3/HashTableClass/HashTableClass/HashTableClass/LinkedList.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableClass
+{
+    class LinkedList<T>
+    {
+        class ListElement
+        {
+            public T Value { get; set; }
+            public ListElement Next { get; set; }
+        }
+
+        /// <summary>
+        /// Add element to list.
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddElement(T value)
+        {
+            var newElement = new ListElement()
+            {
+                Value = value,
+                Next = null
+            };
+
+            if (head == null)
+            {
+                head = newElement;
+                return;
+            }
+
+            ListElement currentElement = head;
+            while (currentElement.Next != null)
+            {
+                currentElement = currentElement.Next;
+            }
+            currentElement.Next = newElement;     
+        }
+
+        /// <summary>
+        /// Add element if it is not in list.
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddUniqueElement(T value)
+        {
+            var newElement = new ListElement()
+            {
+                Value = value,
+                Next = null
+            };
+
+            if (head == null)
+            {
+                head = newElement;
+                return;
+            }
+
+            ListElement currentElement = head;
+            Comparer<T> comparer = Comparer<T>.Default;
+            while (currentElement.Next != null && comparer.Compare(currentElement.Value, value) != 0)
+            {
+                currentElement = currentElement.Next;
+            }
+            if (comparer.Compare(currentElement.Value, value) == 0)
+            {
+                return;
+            }
+            currentElement.Next = newElement;     
+        }
+
+        /// <summary>
+        /// Remove element from list.
+        /// </summary>
+        /// <returns></returns>
+        public T RemoveElementFromTail()
+        {
+            if (head == null)
+            {
+                throw new RemoveFromEmptyListException("You tried to remove element from empty list.");
+            }
+
+            T neededValue;
+
+            if (head.Next == null)
+            {
+                neededValue = head.Value;
+                head = null;
+                return neededValue;
+            }
+
+            ListElement currentElement = head;
+            while (currentElement.Next.Next != null)
+            {
+                currentElement = currentElement.Next;
+            }
+            neededValue = currentElement.Next.Value;
+            currentElement.Next = null;
+            return neededValue;
+        }
+
+        /// <summary>
+        /// Delete element with specified value from list if it is in list.
+        /// </summary>
+        /// <param name="value"></param>
+        public void DeleteElement(T value)
+        {
+            if (head == null)
+            {
+                return;
+            }
+
+            Comparer<T> comparer = Comparer<T>.Default;
+            if (comparer.Compare(head.Value, value) == 0)
+            {
+                head = head.Next;
+            }
+
+            ListElement currentElement = head;
+            while (comparer.Compare(currentElement.Next.Value, value) != 0)
+            {
+                currentElement = currentElement.Next;
+            }
+            currentElement.Next = currentElement.Next.Next;
+        }
+
+        /// <summary>
+        /// Check if element with specified value in list.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool IsElementInList(T value)
+        {
+            var currentElement = head;
+            Comparer<T> comparer = Comparer<T>.Default;
+            while (currentElement!=null && comparer.Compare(currentElement.Value, value) != 0)
+            {
+                currentElement = currentElement.Next;
+            }
+            return currentElement != null;
+        }
+
+        private ListElement head = null;
+    }
+}
