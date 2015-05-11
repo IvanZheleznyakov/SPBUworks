ed511b0831eb3c3f74262170b2bbdbdeef35ddfc
IvanZheleznyakov <iz14@yandex.ru>
2015-05-11 17:05:41 +0400

    Wrote the first part without tests

---
+++ Semester2/HW_2/Task2/LinkedListClass/LinkedListClass/LinkedListClass.Tests/LinkedListClassTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinkedListClass.Tests
+{
+    [TestClass]
+    public class LinkedListClassTest
+    {
+        [TestMethod]
+        public void IsListEmptyTest()
+        {
+            LinkedList list = new LinkedList();
+            Assert.AreEqual(true, list.IsListEmpty());
+        }
+    }
+}
+            //LinkedList list = new LinkedList();
+            //list.AddElement(3);
+            //list.AddElement(5);
+            //list.AddElement(7);
+            //Console.WriteLine(list.RemoveElement());
+            //list.AddElement(9);
+            //Console.WriteLine(list.RemoveElement());
+            //list.AddElement(11);
+            //Console.WriteLine(list.RemoveElement());
+            //Console.WriteLine(list.RemoveElement());
+            //Console.WriteLine(list.RemoveElement());
+            //Console.WriteLine(list.RemoveElement());
---
+++ Semester2/HW_2/Task2/LinkedListClass/LinkedListClass/LinkedListClass/LinkedList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListClass
+{
+    public class LinkedList
+    {
+        public class ListElement
+        {
+            public int Value { get; set; }
+            public ListElement Next { get; set; }
+        }
+
+        /// <summary>
+        /// Add element to list.
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddElement(int value)
+        {
+            var newElement = new ListElement()
+            {
+                Value = value,
+                Next = null
+            };
+            if (head == null)
+            {
+                head = newElement;
+                return;
+            }
+            ListElement currentElement = head;
+            while (currentElement.Next != null)
+            {
+                currentElement = currentElement.Next;
+            }
+            currentElement.Next = newElement;            
+        }
+
+        /// <summary>
+        /// Remove element from list.
+        /// </summary>
+        /// <returns></returns>
+        public int RemoveElement()
+        {
+            if (head == null)
+            {
+                throw new RemoveFromEmptyListException("You tried to remove element from empty list.");
+            }
+
+            int neededValue;
+
+            if (head.Next == null)
+            {
+                neededValue = head.Value;
+                head = null;
+                return neededValue;
+            }
+
+            ListElement currentElement = head;
+            while (currentElement.Next.Next != null)
+            {
+                currentElement = currentElement.Next;
+            }
+            neededValue = currentElement.Next.Value;
+            currentElement.Next = null;
+            return neededValue;
+        }
+
+        /// <summary>
+        /// Check if list is empty.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsListEmpty()
+        {
+            return head == null;
+        }
+
+        private ListElement head = null;
+    }
+}
