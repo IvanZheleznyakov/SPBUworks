b0d881fb69ecc02456ebdcf88a081473558bc483
IvanZheleznyakov <iz14@yandex.ru>
2015-05-12 19:02:16 +0400

    Fixed come bugs with hashtable's initialization

---
+++ Semester2/HW_2/Task3/HashTableClass/HashTableClass/HashTableClass/HashTable.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableClass
+{
+    public class HashTable
+    {
+        public HashTable(int capacity = 50)
+        {
+            Capacity = capacity;
+            for (int i = 0; i != capacity; ++i)
+            {
+                HashTableArray[i] = new MyLinkedList<string>();
+            }
+        }
+
+        /// <summary>
+        /// Count hashfunction for specified value.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public int HashFunction(string value)
+        {
+            int result = 0;
+            for (int i = 0; i != value.Length; ++i)
+            {
+                result += (int)value[i];
+            }
+            return result % Capacity;
+        }
+
+        /// <summary>
+        /// Add element to hashtable.
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddElement(string value)
+        {
+            int index = HashFunction(value);
+            HashTableArray[index].AddUniqueElement(value);
+        }
+
+        /// <summary>
+        /// Delete element with specified value from hashtable.
+        /// </summary>
+        /// <param name="value"></param>
+        public void DeleteElement(string value)
+        {
+            int index = HashFunction(value);
+            HashTableArray[index].DeleteElement(value);
+        }
+
+        /// <summary>
+        /// Check if element with specified value is in list.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool IsElementInHashTable(string value)
+        {
+            int index = HashFunction(value);
+            return HashTableArray[index].IsElementInList(value);
+        }
+
+        public static int Capacity = 50;
+        public MyLinkedList<string>[] HashTableArray = new MyLinkedList<string>[Capacity];
+    }
+}
