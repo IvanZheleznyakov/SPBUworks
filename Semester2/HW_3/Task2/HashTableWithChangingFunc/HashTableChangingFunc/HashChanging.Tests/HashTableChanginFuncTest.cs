b2dc22ebc0117aa8cc4dd52fa4427bf111bb538c
IvanZheleznyakov <iz14@yandex.ru>
2015-05-13 13:03:14 +0400

    Added possibility to change hashfunction

---
+++ Semester2/HW_3/Task2/HashTableWithChangingFunc/HashTableChangingFunc/HashChanging.Tests/HashTableChanginFuncTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HashChanging.Tests
+{
+    [TestClass]
+    public class HashTableChanginFuncTest
+    {
+        [TestMethod]
+        public void AddElementsTest()
+        {
+            var hashTable = new HashTableClass.HashTable();
+            hashTable.AddElement("privet");
+            hashTable.AddElement("yaSdam");
+            hashTable.AddElement("progu");
+            Assert.AreEqual(true, hashTable.IsElementInHashTable("yaSdam"));
+            Assert.AreEqual(true, hashTable.IsElementInHashTable("progu"));
+            Assert.AreEqual(true, hashTable.IsElementInHashTable("privet"));
+        }
+
+        [TestMethod]
+        public void ElementsAbsenceTest()
+        {
+            var hashTable = new HashTableClass.HashTable();
+            hashTable.AddElement("sdam");
+            Assert.AreEqual(false, hashTable.IsElementInHashTable("neSdash"));
+        }
+
+        [TestMethod]
+        public void DeleteElementTest()
+        {
+            var hashTable = new HashTableClass.HashTable();
+            hashTable.AddElement("deleted");
+            hashTable.DeleteElement("deleted");
+            Assert.AreEqual(false, hashTable.IsElementInHashTable("deleted"));
+        }
+    }
+}
---
+++ Semester2/HW_3/Task2/HashTableWithChangingFunc/HashTableChangingFunc/HashTableChangingFunc/HashTable.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableClass
+{
+    public class HashTable
+    {
+        public HashTable(int capacity = 50)
+        {
+            Capacity = capacity;
+            for (int i = 0; i != capacity; ++i)
+            {
+                HashTableArray[i] = new MyLinkedList<string>();
+            }
+        }
+
+        public HashTable(UsingHashFunction hashFunc, int capacity = 50)
+        {
+            Capacity = capacity;
+            for (int i = 0; i != capacity; ++i)
+            {
+                HashTableArray[i] = new MyLinkedList<string>();
+            }
+            usingHashFunc = hashFunc;
+        }
+
+        /// <summary>
+        /// Count default hashfunction for specified value.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private static int DefaultHashFunction(string value)
+        {
+            int result = 0;
+            for (int i = 0; i != value.Length; ++i)
+            {
+                result += (int)value[i];
+            }
+            return result % Capacity;
+        }
+
+        /// <summary>
+        /// Add element to hashtable.
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddElement(string value)
+        {
+            int index = usingHashFunc(value);
+            HashTableArray[index].AddUniqueElement(value);
+        }
+
+        /// <summary>
+        /// Delete element with specified value from hashtable.
+        /// </summary>
+        /// <param name="value"></param>
+        public void DeleteElement(string value)
+        {
+            int index = usingHashFunc(value);
+            HashTableArray[index].DeleteElement(value);
+        }
+
+        /// <summary>
+        /// Check if element with specified value is in list.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool IsElementInHashTable(string value)
+        {
+            int index = usingHashFunc(value);
+            return HashTableArray[index].IsElementInList(value);
+        }
+
+        public delegate int UsingHashFunction(string value);
+
+        public UsingHashFunction usingHashFunc = DefaultHashFunction;
+        public static int Capacity = 50;
+        public MyLinkedList<string>[] HashTableArray = new MyLinkedList<string>[Capacity];
+    }
+}
