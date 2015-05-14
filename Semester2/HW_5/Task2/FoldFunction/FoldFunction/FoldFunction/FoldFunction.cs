a6e0fdbedc5b40cc00257d0aadbf3bc1357da77a
IvanZheleznyakov <iz14@yandex.ru>
2015-05-14 16:59:24 +0400

    All is done

---
+++ Semester2/HW_5/Task2/FoldFunction/FoldFunction/FoldFunction/FoldFunction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoldFunction
+{
+    public class FoldFunction<T>
+    {
+        public static List<T> Fold(List<T> list, Func<T, bool> function)
+        {
+            var resultList = new List<T>();
+
+            foreach(T element in list)
+            {
+                if (function(element))
+                {
+                    resultList.Add(element);
+                }
+            }
+
+            return resultList;
+        }
+    }
+}
