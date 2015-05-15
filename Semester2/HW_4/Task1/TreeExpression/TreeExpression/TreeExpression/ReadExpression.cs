cbea64d963740767b3a1b3399e911e410b43192c
IvanZheleznyakov <iz14@yandex.ru>
2015-05-15 19:37:50 +0400

    Add class ReadExpression

---
+++ Semester2/HW_4/Task1/TreeExpression/TreeExpression/TreeExpression/ReadExpression.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeExpression
+{
+    public class ReadExpression
+    {
+        public void ReadExpressionFromString(Tree tree, string expression)
+        {
+            while (expression.Length != 0)
+            {
+                string OperatorOrOperand = expression.Remove(expression.IndexOf(' '));
+
+                switch (OperatorOrOperand)
+                {
+                    case "+":
+                    case "-":
+                    case "*":
+                    case "/":
+                        tree.AddTreeElement(OperatorOrOperand[0]);
+                        break;
+                    default:
+                        tree.AddTreeElement(Int32.Parse(OperatorOrOperand));
+                        break;
+                }
+
+                expression = expression.Remove(0, OperatorOrOperand.Length);
+                if (expression.Length != 1)
+                {
+                    expression = expression.Remove(0, 1);
+                }
+            }
+        }
+    }
+}
