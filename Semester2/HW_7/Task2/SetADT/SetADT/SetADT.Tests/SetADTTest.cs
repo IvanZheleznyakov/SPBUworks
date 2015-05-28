257acf337bf7d41231c544f7548d49848f2e5c05
IvanZheleznyakov <iz14@yandex.ru>
2015-05-28 18:12:43 +0400

    Working ADT set with tests

---
+++ Semester2/HW_7/Task2/SetADT/SetADT/SetADT.Tests/SetADTTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SetADT.Tests
+{
+    [TestClass]
+    public class SetADTTest
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            var set = new Set<int>();
+            set.AddElement(2);
+            set.AddElement(1);
+            Assert.AreEqual(true, set.IsInSet(2));
+            Assert.AreEqual(true, set.IsInSet(1));
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            var set = new Set<int>();
+            set.AddElement(2);
+            set.AddElement(1);
+            set.DeleteElement(2);
+            Assert.AreEqual(false, set.IsInSet(2));
+            Assert.AreEqual(false, set.IsInSet(-2));
+        }
+
+        [TestMethod]
+        public void UnionTest()
+        {
+            var firstSet = new Set<int>();
+            var secondSet = new Set<int>();
+            firstSet.AddElement(1);
+            firstSet.AddElement(3);
+            firstSet.AddElement(5);
+            secondSet.AddElement(2);
+            secondSet.AddElement(4);
+            var resultTest = Set<int>.Union(firstSet, secondSet);
+            Assert.AreEqual(true, resultTest.IsInSet(1));
+            Assert.AreEqual(true, resultTest.IsInSet(2));
+            Assert.AreEqual(true, resultTest.IsInSet(3));
+            Assert.AreEqual(true, resultTest.IsInSet(4));
+            Assert.AreEqual(true, resultTest.IsInSet(5));
+        }
+
+        [TestMethod]
+        public void IntersectionTest()
+        {
+            var firstSet = new Set<int>();
+            var secondSet = new Set<int>();
+            firstSet.AddElement(3);
+            firstSet.AddElement(1);
+            firstSet.AddElement(2);
+            firstSet.AddElement(5);
+            secondSet.AddElement(5);
+            secondSet.AddElement(1);
+            var resultTest = Set<int>.Intersection(firstSet, secondSet);
+            Assert.AreEqual(true, resultTest.IsInSet(1));
+            Assert.AreEqual(false, resultTest.IsInSet(2));
+            Assert.AreEqual(false, resultTest.IsInSet(4));
+            Assert.AreEqual(true, resultTest.IsInSet(5));
+        }
+    }
+}
---
+++ Semester2/HW_7/Task2/SetADT/SetADT/SetADT/Set.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetADT
+{
+    /// <summary>
+    /// Generic set class.
+    /// </summary>
+    public class Set<T>
+    {
+        private List<T> set;
+
+        public Set()
+        {
+            set = new List<T>();
+        }
+
+        /// <summary>
+        /// Check if element in set.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool IsInSet(T value)
+        {
+            Comparer<T> comparer = Comparer<T>.Default;
+            return set.Exists(x => comparer.Compare(x, value) == 0);
+        }
+
+        /// <summary>
+        /// Add unique element to set.
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddElement(T value)
+        {
+            if (!IsInSet(value))
+            {
+                set.Add(value);
+            }
+        }
+
+        /// <summary>
+        /// Delete element from set.
+        /// </summary>
+        /// <param name="value"></param>
+        public void DeleteElement(T value)
+        {
+            if (IsInSet(value))
+            {
+                set.Remove(value);
+            }
+        }
+
+        /// <summary>
+        /// Union two sets.
+        /// </summary>
+        /// <param name="firstSet"></param>
+        /// <param name="secondSet"></param>
+        /// <returns></returns>
+        public static Set<T> Union(Set<T> firstSet, Set<T> secondSet)
+        {
+            var resultSet = new Set<T>();
+            foreach (T x in firstSet.set)
+            {
+                resultSet.AddElement(x);
+            }
+            foreach (T x in secondSet.set)
+            {
+                resultSet.AddElement(x);
+            }
+            return resultSet;
+        }
+
+        /// <summary>
+        /// Intersections of two sets.
+        /// </summary>
+        /// <param name="firstSet"></param>
+        /// <param name="secondSet"></param>
+        /// <returns></returns>
+        public static Set<T> Intersection(Set<T> firstSet, Set<T> secondSet)
+        {
+            var resultSet = new Set<T>();
+            foreach (T x in firstSet.set)
+            {
+                if (secondSet.IsInSet(x))
+                {
+                    resultSet.AddElement(x);
+                }
+            }
+            return resultSet;
+        }
+    }
+}
