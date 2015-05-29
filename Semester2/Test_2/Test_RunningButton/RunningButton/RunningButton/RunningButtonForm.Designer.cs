e116f7dab65bcf3f8dd6e5aabd006ed342155a55
IvanZheleznyakov <iz14@yandex.ru>
2015-05-29 14:35:11 +0400

    Working running button

---
+++ Semester2/Test_2/Test_RunningButton/RunningButton/RunningButton/RunningButtonForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace RunningButton
+{
+    partial class RunningButtonForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RunningButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RunningButton
+            // 
+            this.RunningButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RunningButton.Location = new System.Drawing.Point(97, 146);
+            this.RunningButton.Name = "RunningButton";
+            this.RunningButton.Size = new System.Drawing.Size(75, 23);
+            this.RunningButton.TabIndex = 0;
+            this.RunningButton.TabStop = false;
+            this.RunningButton.Text = "YOU SHALL NOT CATCH ME";
+            this.RunningButton.UseVisualStyleBackColor = true;
+            this.RunningButton.Click += new System.EventHandler(this.OnRunningButtonClick);
+            this.RunningButton.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MouseMoveOnRunningButton);
+            // 
+            // RunningButtonForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.RunningButton);
+            this.MaximumSize = new System.Drawing.Size(400, 400);
+            this.MinimumSize = new System.Drawing.Size(400, 400);
+            this.Name = "RunningButtonForm";
+            this.Text = "RunningButton";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RunningButton;
+    }
+}
+
---
+++ Semester2/Test_2/Test_RunningButton/RunningButton/RunningButton/RunningButtonForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunningButton
+{
+    public partial class RunningButtonForm : Form
+    {
+        public RunningButtonForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MouseMoveOnRunningButton(object sender, MouseEventArgs e)
+        {
+            Button button = sender as Button;
+            var randomNumber = new Random();
+            int newX = randomNumber.Next(0, this.ClientSize.Width - button.Width);
+            int newY = randomNumber.Next(0, this.ClientSize.Height - button.Height);
+            button.Location = new System.Drawing.Point(newX, newY);
+        }
+
+        private void OnRunningButtonClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("YOU SHALL NOT PASS THE EXAM ):");
+        }
+    }
+}
