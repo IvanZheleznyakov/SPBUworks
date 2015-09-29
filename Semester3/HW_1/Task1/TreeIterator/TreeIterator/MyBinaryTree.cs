4f6a20fde9f78b5eaec5106cc05bb2d6113d34e9
IvanZheleznyakov <iz14@yandex.ru>
2015-09-29 18:25:26 +0400

    Added tree and function AddElement

---
+++ Semester3/HW_1/Task1/TreeIterator/TreeIterator/MyBinaryTree.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeIterator
+{
+    public class MyBinaryTree<T>
+    {
+        private class TreeElement
+        {
+            public T Value { get; set; }
+            public TreeElement Left { get; set; }
+            public TreeElement Right { get; set; }
+        }
+
+        private TreeElement head = null;
+
+        public void AddElement(T value)
+        {
+            var newElement = new TreeElement()
+            {
+                Value = value,
+                Left = null,
+                Right = null
+            };
+
+            if (head == null)
+            {
+                head = newElement;
+                return;
+            }
+
+            var currentElement = head;
+            Comparer<T> comparer = Comparer<T>.Default;
+            
+            while (currentElement != null)
+            {
+                if (comparer.Compare(newElement.Value, currentElement.Value) <= 0)
+                {
+                    if (currentElement.Left == null)
+                    {
+                        currentElement.Left = newElement;
+                        return;
+                    }
+                    currentElement = currentElement.Left;
+                }
+                else
+                {
+                    if (currentElement.Right == null)
+                    {
+                        currentElement.Right = newElement;
+                        return;
+                    }
+                    currentElement = currentElement.Right;
+                }
+            }
+
+            if (comparer.Compare(newElement.Value, head.Value) <= 0)
+            {
+                head.Left = newElement;
+                return;
+            }
+            head.Right = newElement;
+        }
+    }
+}
--- Semester3/HW_1/Task1/TreeIterator/TreeIterator/Program.cs
+++ Semester3/HW_1/Task1/TreeIterator/TreeIterator/Program.cs
@@ -10,6 +10,7 @@ namespace TreeIterator
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
