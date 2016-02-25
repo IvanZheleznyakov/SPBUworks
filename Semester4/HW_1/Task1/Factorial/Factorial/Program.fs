e5ea7d6662589a10c9168fdf0879a25a39313045
IvanZheleznyakov <iz14@yandex.ru>
2016-02-25 17:46:50 +0400

    first task is done

--- Semester4/HW_1/Task1/Factorial/Factorial/Program.fs
+++ Semester4/HW_1/Task1/Factorial/Factorial/Program.fs
@@ -1,7 +1,10 @@
-﻿// Дополнительные сведения о F# см. на http://fsharp.net
-// Дополнительную справку см. в проекте "Учебник по F#".
+﻿let rec factorial x =
+    match x with
+    | 0 | 1 -> 1
+    | (_) -> x * factorial (x - 1)
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // возвращение целочисленного кода выхода
+let firstTemp = factorial 0
+printfn "Factorial 0: %d" firstTemp
+
+let secondTemp = factorial 9
+printfn "Factorial 9: %d" secondTemp
