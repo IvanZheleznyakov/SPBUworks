a5ec130914e951a416c8b890818d3e0ba54af91f
IvanZheleznyakov <iz14@yandex.ru>
2016-02-26 10:59:28 +0400

    done

---
+++ Semester4/HW_1/Task3/ReverseList/ReverseList/Program.fs
@@ -0,0 +1,11 @@
+﻿let reverseList ls = 
+    let rec reverse firsts seconds =
+        match firsts with
+        | h :: t -> reverse t (h :: seconds)
+        | [] -> seconds
+    reverse ls []
+
+let firstTemp = reverseList [ 1 ]
+let secondTemp = reverseList [ 1; 2; 3; 4; 5; 6; 7 ]
+printfn "[1; 2; 3] - reverse: %A" firstTemp
+printfn "[1; 2; 3; 4; 5; 6; 7] - reverse: %A" secondTemp
