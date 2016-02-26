310e00856f94dea3cdedf605d96d0f938b51030d
IvanZheleznyakov <iz14@yandex.ru>
2016-02-26 10:58:00 +0400

    done

---
+++ Semester4/HW_1/Task2/Fibonacci/Fibonacci/Program.fs
@@ -0,0 +1,9 @@
+﻿let rec fibonacci x =
+    match x with
+    | 0 -> 0
+    | 1 -> 1
+    | (_) -> fibonacci (x - 1) + fibonacci (x - 2)
+
+let firstTemp = fibonacci 4
+let secondTemp = fibonacci 9
+printfn "4th fibonacci is %d \n9th fibonacci is %d" firstTemp secondTemp
