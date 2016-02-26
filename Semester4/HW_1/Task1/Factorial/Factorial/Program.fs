769a5ad245c3373735a874c15907437c89a88aad
IvanZheleznyakov <iz14@yandex.ru>
2016-02-26 10:56:38 +0400

    done

---
+++ Semester4/HW_1/Task1/Factorial/Factorial/Program.fs
@@ -0,0 +1,10 @@
+﻿let rec factorial x =
+    match x with
+    | 0 | 1 -> 1
+    | (_) -> x * factorial (x - 1)
+
+let firstTemp = factorial 0
+printfn "Factorial 0: %d" firstTemp
+
+let secondTemp = factorial 9
+printfn "Factorial 9: %d" secondTemp
