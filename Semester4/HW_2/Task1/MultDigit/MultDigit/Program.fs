62e6914b89ba9b53cf15ffcdfe0d9a281565aa65
IvanZheleznyakov <iz14@yandex.ru>
2016-03-09 22:38:11 +0400

    done

---
+++ Semester4/HW_2/Task1/MultDigit/MultDigit/Program.fs
@@ -0,0 +1,10 @@
+﻿let multDigit x = 
+    let rec inMultDigit y mult =
+        match y with
+        | 0 -> mult
+        | _ -> inMultDigit (y / 10) (mult * (y % 10))
+    match x with
+    | 0 -> 0
+    | _ -> inMultDigit x 1
+
+printfn "%d, %d" <| multDigit 13212511 <| multDigit 0
