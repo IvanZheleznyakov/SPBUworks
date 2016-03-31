8e61225dc26051aa03a594ef9e993f3bce1f0c01
IvanZheleznyakov <iz14@yandex.ru>
2016-04-01 03:03:36 +0400

    3.3 is done

---
+++ Semester4/HW_3/Task3/HeightTree/HeightTree/Program.fs
@@ -0,0 +1,8 @@
+﻿type Tree<'a> =
+    | Tree of 'a * Tree<'a> * Tree<'a>
+    | TerminalV of 'a
+
+let rec heightOfTree tree =
+    match tree with
+    | Tree(_, l, r) -> 1 + max (heightOfTree l) (heightOfTree r)
+    | TerminalV(_) -> 1
