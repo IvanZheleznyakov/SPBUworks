979420470581a323bd74acf23049575039405fd8
IvanZheleznyakov <iz14@yandex.ru>
2016-05-12 01:24:21 +0400

    done. again.

---
+++ Semester4/HW_3/Task5/SeqPrime/SeqPrime/Program.fs
@@ -0,0 +1,7 @@
+﻿let isPrime n =
+    let rec check i =
+        i > n / 2 || (n % i <> 0 && check (i + 1))
+    check 2
+
+let mySeq = Seq.initInfinite (fun index -> index + 2) |> Seq.filter(fun x -> isPrime x)
+printfn "inf seq of prime nums: %A" mySeq
