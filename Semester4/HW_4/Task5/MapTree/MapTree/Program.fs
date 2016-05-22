1979cbc2539e8ab1ff2415386eea0ef33dc8f9c5
IvanZheleznyakov <iz14@yandex.ru>
2016-05-22 16:59:50 +0400

    done

---
+++ Semester4/HW_4/Task5/MapTree/MapTree/Program.fs
@@ -0,0 +1,8 @@
+﻿type Tree<'a> =
+    | Tree of 'a * Tree<'a> * Tree<'a>
+    | TerminalV of 'a
+
+let rec mapTree tree func = 
+    match tree with
+    | Tree(value, left, right) -> Tree(func value, mapTree left func, mapTree right func)
+    | TerminalV(value) -> TerminalV(func value)
