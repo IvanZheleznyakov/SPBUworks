8c3557dd2d69c87119af19e8f6a684b964b3b2d7
IvanZheleznyakov <iz14@yandex.ru>
2016-05-23 01:59:13 +0400

    regex doesnt work

---
+++ Semester4/HW_7/Task1/WebPages/WebPages/Program.fs
@@ -0,0 +1,27 @@
+﻿open System.Text.RegularExpressions
+open System.Net
+open System.IO
+
+let getInfo(url: string) = 
+    let getHtml(url : string) =
+        async { 
+            let req = WebRequest.Create(url)
+            use! response = req.AsyncGetResponse()
+            use stream = response.GetResponseStream()
+            use reader = new StreamReader(stream)
+            return reader.ReadToEnd() }
+    let func(url: string) = 
+        Async.RunSynchronously <|
+            async {
+                let! html = getHtml url
+                let regularExpression = new Regex(@"<a href=""http://.+?"">")
+                let matches = regex.Matches(html)
+                let references = [|for i in matches -> i.Value.Substring(9, m.Value.Length - 11)|]
+                let tasks = [|for i in references -> getHtml i|]
+                let! info = Async.Parallel tasks 
+                for i in 0 .. references.Length - 1 do
+                    printfn "%s --- %d" references.[i] info.[i].Length }
+    func url
+
+
+getInfo "http://nba.com"
